b40af2443e60593be3f8fe8a9ac6cdd05a0d009e
Vicente Canales <me+github@devjunk.org>
2016-11-09 15:04:16 -0300

    Remove dist files

--- dist/app.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var _snoocore = require('snoocore');
-
-var _snoocore2 = _interopRequireDefault(_snoocore);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
