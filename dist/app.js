8da44bd13bb923cf1b6cbe8baeddd4bfa256fe84
Vicente Canales <me+github@devjunk.org>
2016-11-09 15:03:38 -0300

    Initial Commit

---
+++ dist/app.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var _snoocore = require('snoocore');
+
+var _snoocore2 = _interopRequireDefault(_snoocore);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
